9711a24a4fcaf3d18af0ef4f21df034a90f8193c
matoototo <furrowedsip@gmail.com>
2021-04-14 00:19:07 +0200

    Fix parser.py bug.

--- parser/parser.py
+++ parser/parser.py
@@ -1,6 +1,7 @@
 from io import TextIOWrapper
 import chess.pgn
 import sys
+import os
 
 
 def parse_tc(tc_string):
@@ -11,6 +12,8 @@ def parse_tc(tc_string):
 
 def parse_res(res_string):
     res_string = res_string.split("-")
+    if ("*" in res_string):
+        return None
     if ("1/2" in res_string):
         return "0\n"
     return str(int(res_string[0])-int(res_string[1])) + '\n'
@@ -18,8 +21,9 @@ def parse_res(res_string):
 def parse_game(game : chess.pgn.Game, out : TextIOWrapper):
     if (game != None):
         tc = parse_tc(game.headers["TimeControl"])
-        if (tc == None): return
-        out.write(parse_res(game.headers["Result"]))
+        res = parse_res(game.headers["Result"])
+        if (tc == None or res == None): return
+        out.write(res)
         out.write(game.headers["WhiteElo"] + '\n')
         out.write(game.headers["BlackElo"] + '\n')
         out.write(tc)
@@ -38,12 +42,15 @@ if (len(sys.argv) != 3):
     print("Wrong arguments!\nargs: path-to-input-pgn, path-to-output\n")
     exit(1)
 
-pgn = open(sys.argv[1])
-output = open(sys.argv[2], 'a+')
-i = 0
-game = chess.pgn.read_game(pgn)
-while game != None:
+for pgn_file in os.listdir(sys.argv[1]):
+    print(pgn_file)
+    if "processed_" + pgn_file.split(".")[0].split("_")[-1] + ".data" in os.listdir(sys.argv[2]): continue
+    pgn = open(sys.argv[1] + pgn_file)
+    output = open(sys.argv[2] + "processed_" + pgn_file.split(".")[0].split("_")[-1] + ".data", 'a+')
+    i = 0
     game = chess.pgn.read_game(pgn)
-    parse_game(game, output)
-    i += 1
-print(i)
+    while game != None:
+        parse_game(game, output)
+        game = chess.pgn.read_game(pgn)
+        i += 1
+    print(i)
