c8a860ca2f13d19fddcfa9e162ba050ccee043e0
matoototo <furrowedsip@gmail.com>
2021-07-19 19:23:29 +0200

    Make assign_elo a callable script.

--- cc/utilities/assign_elo.py
+++ cc/utilities/assign_elo.py
@@ -1,5 +1,7 @@
 import torch
 from cc.parser.data import PositionDataset, tc_to_plane, elo_to_plane
+from cc.utilities.evaluator import load_net
+import argparse, pathlib, json
 
 EPS = 0.05
 
@@ -28,3 +30,20 @@ def naive_assign(net, dataset : PositionDataset, target = 0.2, tc = 600):
                 assigned.append({ 'fen': dataset.positions[i].fen, 'elo': i_to_elo(min), 'eval': dataset.positions[i].eval})
     return assigned
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Assign Elo to positions.')
+    parser.add_argument('-i', '--input', metavar='path', type=pathlib.Path, help='the path to the input .data file', required=True)
+    parser.add_argument('-o', '--output', metavar='path', type=pathlib.Path, help='the path to the output .json file', required=True)
+    parser.add_argument('--net', metavar='path', type=pathlib.Path, help='the network weights used to assign Elo', required=True)
+    parser.add_argument('--target', metavar='value', type=float, help='the target used to assign the Elo', required=False, default=0.15)
+    parser.add_argument('--tc', metavar='value', type=float, help='the time control used to assign the Elo', required=False, default=1200.0)
+    parser.add_argument('-l', '--limit', metavar='value', type=int, help='only assign Elo to the first l', required=False)
+
+    args = parser.parse_args()
+
+    dataset = PositionDataset(args.input)
+    dataset.parse_data(args.limit)
+
+    assigned = naive_assign(load_net(args.net), dataset, args.target, args.tc)
+    assigned.sort(key = lambda x : x['elo'])
+    json.dump(assigned, open(args.output, 'w+'), indent=4)
